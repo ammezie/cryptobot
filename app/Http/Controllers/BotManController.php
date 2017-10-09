b3306eeff1d5654929c5437c8306aacf685254e5
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-10-09 10:33:02 +0100

    fix typo in spellings

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -17,14 +17,14 @@ class BotManController extends Controller
     {
         $botman = app('botman');
 
-        $botman->hears('crytocompare {limit}', function ($bot, $limit) {
-            $result = $this->compareCrytocurrencies($limit);
+        $botman->hears('cryptocompare {limit}', function ($bot, $limit) {
+            $result = $this->compareCryptocurrencies($limit);
             $bot->reply($result);
         });
 
         // Fallback in case of wrong command
         $botman->fallback(function ($bot) {
-            $bot->reply("Sorry, I did not understand these commands. Try: `crytocompare 5`");
+            $bot->reply("Sorry, I did not understand these commands. Try: `cryptocompare 5`");
         });
 
         $botman->listen();
@@ -39,19 +39,19 @@ class BotManController extends Controller
     }
 
     /**
-     * Get comparison of crytocurrencies from an API
+     * Get comparison of cryptocurrencies from an API
      *
      * @param integer $limit
      * @return string
      */
-    protected function compareCrytocurrencies($limit)
+    protected function compareCryptocurrencies($limit)
     {
         $client = new Client(['base_uri' => 'https://api.coinmarketcap.com/v1/ticker/']);
 
         $response = $client->get('?limit=' . $limit);
         $results = json_decode($response->getBody()->getContents());
 
-        $data = "Here' s the comparison of the top $limit crytocurrencies: " . PHP_EOL;
+        $data = "Here's the comparison of the top $limit cryptocurrencies: " . PHP_EOL;
 
         foreach ($results as $result) {
             $data .= '> ' . $result->name . ' | ' . $result->symbol . ' | ' . '$' . $result->price_usd . ' | ' . '$' . $result->market_cap_usd . PHP_EOL;
